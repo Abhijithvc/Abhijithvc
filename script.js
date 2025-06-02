193d23ac2b7bc79479f2eeece7ae421cdf60027f
Abhijithvc <abhijithvbm95@gmail.com>
2025-06-02 16:47:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,20 +35,13 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["Animation", "Motion Animation", "3D", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
-    var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-
-    // owl carousel script
+      // owl carousel script
     $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
